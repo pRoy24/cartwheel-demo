014bda1798029d7cf48e6b460683b3e8061c30ee
Pritam Roy <roypritam1234@gmail.com>
2019-02-06 16:18:18 +0000

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatFormFieldModule, MatRippleModule, MatInputModule, MatListModule} from '@angular/material';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+const modules = [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRippleModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatListModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...modules
   ],
+
+  exports: [...modules],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
